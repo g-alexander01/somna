e70a99f964490b7a1691e56f10ee1807346d0dcf
g-alexander01 <92596466+g-alexander01@users.noreply.github.com>
2024-12-16 18:45:55 +0000

    Removed sensitive data

--- web_app/app.py
+++ web_app/app.py
@@ -417,4 +417,4 @@ def render_page(tab):
 
 #################################### RUN APP ####################################
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server()
